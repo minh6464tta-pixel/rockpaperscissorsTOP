66c8c071b2aea4e156ea6739e7df89c7b846096d
minh6464tta-pixel <minh6464tta@gmail.com>
2026-04-22 21:51:22 +0700

    Add initial files and pseudo codes

---
+++ rps.js
@@ -0,0 +1,10 @@
+//1. Logic for computer choice
+
+//Randomly generate a number from 1 to 3
+
+//2, Logic for Human choice
+
+//Ask user to input a Rock, Paper or Scissors
+
+console.log("Hello world!")
+
